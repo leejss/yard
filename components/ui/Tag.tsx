bccb76046fba7b7f0188dd702c26debe9fb2386b
Jake <jake.lee@jalsalda.com>
2022-08-18 00:11:59 +0900

    Add Storybook and bye Ghost

--- components/ui/Tag.tsx
+++ components/ui/Tag.tsx
@@ -1,5 +1,11 @@
-const Tag = () => {
-  return <div>Tags</div>;
+import type { ReactNode } from "react";
+
+interface TagProps {
+  children: ReactNode;
+}
+
+const Tag = ({ children }: TagProps) => {
+  return <span className="bg-indigo-600">{children}</span>;
 };
 
 export default Tag;
