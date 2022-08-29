009c4056de6dbe7c62b6fff797789d1fd521c2ec
leejss <goldemshine@gmail.com>
2022-08-29 23:57:25 +0900

    Change Client

--- lib/client.ts
+++ lib/client.ts
@@ -1,5 +1,12 @@
 // import StoryBlok from "storyblok-js-client";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import { API_TOKEN } from "lib/constants";
 import { getStoryblokApi } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: API_TOKEN,
+  use: [apiPlugin],
+});
 
 const ApiClient = getStoryblokApi();
 
