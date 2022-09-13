8f17db8bde7b77e82a13b8d7aa76c453baf45a3b
leejss <goldemshine@gmail.com>
2022-09-13 17:29:37 +0900

    s

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head />
-
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&display=swap"
         rel="stylesheet"
