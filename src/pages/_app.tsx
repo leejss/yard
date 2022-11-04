a406fba48940c8f8d34e16fb217b8a34c4a3cea6
leejss <goldemshine@gmail.com>
2022-11-04 20:34:14 +0900

    fix: tailwindcss config error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import Layout from "@components/Layout";
 import type { AppProps } from "next/app";
 import type { NextPage } from "next";
-import "../styles/globals.css";
 import type { ReactElement, ReactNode } from "react";
+import "../styles/globals.css";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
