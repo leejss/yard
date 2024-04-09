aab3be6c6db053978de851733d73e2bc0bdf99f6
leejss <goldemshine@gmail.com>
2024-04-09 13:38:07 +0900

    Add biome

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,5 @@
-import {
-  type LinksFunction,
-  type LoaderFunctionArgs,
-  json,
-} from "@remix-run/node";
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { type LinksFunction, type LoaderFunctionArgs, json } from "@remix-run/node";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import FixedNavbar from "~/components/fixed-navbar";
 import { useTheme } from "~/hooks/useTheme";
 import stylehseet from "~/styles/tailwind.css?url";
@@ -17,45 +7,45 @@ import { cn } from "~/utils";
 import { getThemeFromRequest } from "~/utils/get-theme";
 
 export const links: LinksFunction = () => [
-  {
-    rel: "stylesheet",
-    href: stylehseet,
-  },
+	{
+		rel: "stylesheet",
+		href: stylehseet,
+	},
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const data = {
-    userPreference: {
-      theme: await getThemeFromRequest(request),
-    },
-  };
+	const data = {
+		userPreference: {
+			theme: await getThemeFromRequest(request),
+		},
+	};
 
-  return json(data);
+	return json(data);
 };
 
 export type RootLoader = typeof loader;
 export function Layout({ children }: { children: React.ReactNode }) {
-  const theme = useTheme();
-  return (
-    <html lang="en" className={cn(theme)}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="antialiased">
-        <FixedNavbar />
-        <main className="mt-nav_height container mx-auto px-4">{children}</main>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	const theme = useTheme();
+	return (
+		<html lang="en" className={cn(theme)}>
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="antialiased">
+				<FixedNavbar />
+				<main className="mt-nav_height container mx-auto px-4">{children}</main>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
 
 // Add theme className to html and listen to the html className
