5c0c24b3f671efbd13e404525c0c4f9665981000
leejss <goldemshine@gmail.com>
2024-04-10 11:30:32 +0900

    Update FixedNavbar component styling and add media query for larger screens

--- app/components/fixed-navbar.tsx
+++ app/components/fixed-navbar.tsx
@@ -6,10 +6,10 @@ const FixedNavbar = () => {
 			<nav className="h-nav_height container mx-auto px-4 bg-red-500 rounded-b-lg text-white">
 				<div className="flex justify-between h-full items-center">
 					<Link to="/">
-						<h1 className="text-3xl font-bold font-mono">tinyyard</h1>
+						<h1 className="text-3xl md:text-4xl font-bold font-mono">tinyyard</h1>
 					</Link>
 
-					<div className="flex items-center gap-4">
+					<div className="flex items-center gap-4 text-lg md:text-xl">
 						<Link className="font-bold" to="/posts">
 							Posts
 						</Link>
